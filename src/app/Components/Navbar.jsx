92dfaf37ea1b8f6bb7df5cc98aaf8745292d5006
euukc <keniacarolineferreira@gmail.com>
2024-07-23 19:23:02 -0300

    .

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -7,17 +7,14 @@ export default function Navbar() {
 
     const clickBtnWpp = () => {
         const phoneNumber = '5511994929467';
-
         const message = `Olá, tenho interesse em solicitar um orçamento!`;
-
         const encodedMessage = encodeURI(message);
         const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
-
         window.open(url, '_blank');
     };
 
     return (
-        <section className="flex justify-center items-center flex-col w-screen mt-[120px]">
+        <section className="flex justify-center items-center flex-col w-screen mt-[120px] mb-[70px]">
             <div className="flex flex-col justify-center items-center text-center">
                 <Image src="/Kenia_Caroline1.jpeg" width={150} height={100} alt="foto da kênia" className="rounded-[200px]" />
                 <h1 className="mt-[20px] text-[40px]">Kênia Caroline</h1>
@@ -28,29 +25,28 @@ export default function Navbar() {
                 <div className="botoes-links flex ">
                     <Image className="text-start" src='/alien-icon.png' width={20} height={10} />
                     <div style={{ flex: 1, textAlign: 'center' }}>
-                    <Link href="https://keniadevportfolio.vercel.app/" target="_blank">Meu Site Portfólio</Link>
+                        <Link href="https://keniadevportfolio.vercel.app/" target="_blank">Meu Site Portfólio</Link>
                     </div>
                 </div>
                 <div className="botoes-links flex">
                     <Image src='/alien-icon.png' width={20} height={10} />
                     <div style={{ flex: 1, textAlign: 'center' }}>
-                    <Link href="https://www.linkedin.com/in/keniac/" target="_blank">Linkedin</Link>
+                        <Link href="https://www.linkedin.com/in/keniac/" target="_blank">Linkedin</Link>
                     </div>
                 </div>
                 <div className="botoes-links flex">
                     <Image src='/alien-icon.png' width={20} height={10} />
                     <div style={{ flex: 1, textAlign: 'center' }}>
-                    <Link href="https://github.com/euukc" target="_blank">Github</Link>
+                        <Link href="https://github.com/euukc" target="_blank">Github</Link>
                     </div>
                 </div>
                 <div className="botoes-links flex">
                     <Image src='/alien-icon.png' width={20} height={10} />
                     <div style={{ flex: 1, textAlign: 'center' }}>
-                    <Link onClick={clickBtnWpp} className="text-[16px] md:text-[18px]" href="/" target="_blank">Entre em contato comigo</Link>
+                        <Link onClick={clickBtnWpp} className="text-[16px] md:text-[18px]" href="/" target="_blank">Entre em contato comigo</Link>
                     </div>
                 </div>
             </div>
-
         </section>
     )
 }
